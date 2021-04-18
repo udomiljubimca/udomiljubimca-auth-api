e0982e1168ac565368fc5c509611b0e5475494d6
dvitkovic <djordje.vitkovic@namics.com>
2021-04-19 00:03:49 +0200

    Refactoring, added Lombok, Service layer, get token by refresh token, added some todos

--- src/main/java/com/auth/testlogin/model/UserCredentials.java
+++ src/main/java/com/auth/testlogin/model/UserCredentials.java
@@ -1,25 +1,18 @@
 package com.auth.testlogin.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserCredentials {
 
 	private String password;
 
 	private String username;
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 
 }
